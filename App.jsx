6e5b5a132f6236f894d60ca2feda2356607145fb
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-03-15 07:46:24 +0530

    create-react-app &configure tailwind css

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+      <h1 className='text-2xl font-red text-green-800'>Namste EveryOne</h1>
+     
+    </>
+  )
+}
+
+export default App

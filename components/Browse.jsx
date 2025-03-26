a5a4107032cf5a187d0500c339f38f47d22bd214
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-03-26 23:58:16 +0530

    feat:TMDB NOW PLAYING MOVIES OPTION ADDED

--- components/Browse.jsx
+++ components/Browse.jsx
@@ -1,7 +1,17 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import { API_Options } from "../utils/constants";
 const Browse =()=>{
+    const getNowPlayingApi=async()=>{
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',API_Options)
+        const json =data.json();
+        console.log(json)
+    }
+
+    useEffect(()=>{
+        getNowPlayingApi() 
+    },[])
     return(
         <div>
             {/* <Header /> */}
